3c936ed19ade7d32208c2d9530424f3c6521edf3
Gajanand <gajanand@Gajanands-MacBook-Pro.local>
2024-10-26 15:17:59 +0530

    koin integartion - app module configured for dependency injection

---
+++ app/src/main/java/com/learning/cryptotracker/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.learning.cryptotracker.di
+
+import com.learning.cryptotracker.core.data.networking.HttpClientFactory
+import com.learning.cryptotracker.crypto.data.networking.RemoteCoinDataSource
+import com.learning.cryptotracker.crypto.domain.CoinDataSource
+import com.learning.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
+import io.ktor.client.engine.cio.CIO
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+val appModule = module {
+    single { HttpClientFactory.create(CIO.create()) }
+    //both below works fine
+//    single { RemoteCoinDataSource(get()) }.bind<CoinDataSource>()
+    singleOf(::RemoteCoinDataSource).bind<CoinDataSource>()
+    viewModelOf(::CoinListViewModel)
+}
